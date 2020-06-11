ad43dc1feaeeb97c84415bf10b1f46ba8a32c67f
unknown <00159619@uca.edu.sv>
2020-06-10 18:43:27 -0600

    Add: static class ControlJuego.cs

---
+++ Source Code/Source Code/ControlJuego.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Source_Code
+{
+    static class ControlJuego
+    {
+        static private int _vidas, _score = 0, _level, _timer;
+        static public int vidas { get; set; }
+        static public int score { get; set; }
+        static public int level { get; set; }
+        static public int timer { get; set; }
+    }
+}
