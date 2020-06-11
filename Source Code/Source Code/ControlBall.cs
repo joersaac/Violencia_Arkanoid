d08d3c96c9e5513d91770d6e0626b3de18e250ad
unknown <00159619@uca.edu.sv>
2020-06-10 22:27:56 -0600

    Add: static class ControlBall.cs

---
+++ Source Code/Source Code/ControlBall.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Source_Code
+{
+    class ControlBall
+    {
+        //Velocidad horizontal y vertical
+        private static int _vSpeed, _hSpeed;
+
+        public static int vSpeed { get; set; }
+        public static int hSpeed { get; set; }
+    }
+}
