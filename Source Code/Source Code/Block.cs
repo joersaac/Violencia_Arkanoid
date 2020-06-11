0dc46f42a1ebd9316f6243c1608584acda975b9c
unknown <00159619@uca.edu.sv>
2020-06-10 18:48:12 -0600

    Add: class Block.cs

---
+++ Source Code/Source Code/Block.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Source_Code
+{
+    class Block : PictureBox
+    {
+        private int _hitPoints;
+        private int _puntos;
+
+        public Block()
+            : base()
+        {
+            hitPoints = 1;
+            puntos = 10;
+        }
+
+        public int hitPoints { get; set; }
+
+        public int puntos { get; set; }
+    }
+}
