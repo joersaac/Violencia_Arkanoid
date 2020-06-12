dd331955ac388dac42abca4e177e0ba8ae67450c
unknown <00159619@uca.edu.sv>
2020-06-12 13:21:45 -0600

    Modify: added new variable to ControlJuego.cs

--- Source Code/Source Code/ControlJuego.cs
+++ Source Code/Source Code/ControlJuego.cs
@@ -8,18 +8,19 @@ namespace Source_Code
 {
     static class ControlJuego
     {
-        static private int _vidas, _score, _level, _time, _row, _col, _total, _multiplier;
+        static private int _vidas, _score, _level, _time, _row, _col, _total;
         static private bool start;
+        static private string _playerName;
 
         static public int vidas { get; set; }
         static public int score { get; set; }
         static public int level { get; set; }
         static public int timer { get; set; }
         static public int total { get; set; }
-        static public int multiplier { get; set; }
         static public int row { get; set; }
         static public int col { get; set; }
         static public bool started { get; set; }
+        static public string playerName{ get; set; }
 
     }
 }
